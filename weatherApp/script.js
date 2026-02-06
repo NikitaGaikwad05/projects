a539b925b0eca36f5b3a9caca63cccdcb0befb19
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2026-02-07 01:27:13 +0530

    Add files via upload

---
+++ weatherApp/script.js
@@ -0,0 +1,32 @@
+function getWeather() {
+    const location = document.getElementById("locationInput").value;
+
+    if (location === "") {
+        alert("Please enter a location");
+        return;
+    }
+
+    const apiKey = "a856f2e33073481eb22191554260602";
+    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherResult = document.getElementById("weatherResult");
+
+            if (data.error) {
+                weatherResult.innerHTML = `<p>❌ Location not found</p>`;
+            } else {
+                weatherResult.innerHTML = `
+                    <p><strong>📍 Location:</strong> ${data.location.name}, ${data.location.country}</p>
+                    <p><strong>🌡 Temperature:</strong> ${data.current.temp_c} °C</p>
+                    <p><strong>☁ Condition:</strong> ${data.current.condition.text}</p>
+                `;
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            document.getElementById("weatherResult").innerHTML =
+                `<p>⚠ Error fetching data</p>`;
+        });
+}
